6e1ceab61f86f384966a1be294c1928394792976
Josyani Santos <94111206+AniSnt@users.noreply.github.com>
2025-01-21 00:21:25 -0300

    Create LivroService.java

---
+++ LivroService.java
@@ -0,0 +1,36 @@
+package com.josyani.service;
+
+import com.exemplo.model.Livro;
+import com.exemplo.repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LivroService {
+
+    private final LivroRepository livroRepository;
+
+    @Autowired
+    public LivroService(LivroRepository livroRepository) {
+        this.livroRepository = livroRepository;
+    }
+
+    public Livro salvar(Livro livro) {
+        return livroRepository.save(livro);
+    }
+
+    public List<Livro> listarTodos() {
+        return livroRepository.findAll();
+    }
+
+    public Optional<Livro> buscarPorId(Long id) {
+        return livroRepository.findById(id);
+    }
+
+    public void deletar(Long id) {
+        livroRepository.deleteById(id);
+    }
+}
