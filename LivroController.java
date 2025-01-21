b4b5120d1bfc40a81eedf18c5a7a0b788e865357
Josyani Santos <94111206+AniSnt@users.noreply.github.com>
2025-01-21 00:22:28 -0300

    Create LivroController.java

---
+++ LivroController.java
@@ -0,0 +1,47 @@
+package com.josyani.controller;
+
+import com.exemplo.model.Livro;
+import com.exemplo.service.LivroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/livros")
+public class LivroController {
+
+    private final LivroService livroService;
+
+    @Autowired
+    public LivroController(LivroService livroService) {
+        this.livroService = livroService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Livro> criarLivro(@RequestBody Livro livro) {
+        Livro livroSalvo = livroService.salvar(livro);
+        return new ResponseEntity<>(livroSalvo, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Livro>> listarLivros() {
+        List<Livro> livros = livroService.listarTodos();
+        return ResponseEntity.ok(livros);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Livro> buscarLivroPorId(@PathVariable Long id) {
+        Optional<Livro> livro = livroService.buscarPorId(id);
+        return livro.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarLivro(@PathVariable Long id) {
+        livroService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
